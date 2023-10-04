c0b074f915d97f58cd78cf8d94009500c28774e5
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-10-04 17:28:46 -0400

    feat: adding new routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import StorePage from "./pages/StorePage/StorePage.tsx";
+import { AppContextProvider } from "./context/appContext.tsx";
+import Login from "./pages/Login/Login.tsx";
+import Register from "./pages/Register/Register.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <StorePage />,
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AppContextProvider>
+      <RouterProvider router={router} />
+    </AppContextProvider>
   </React.StrictMode>
 );
